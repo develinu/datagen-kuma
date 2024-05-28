c30b832ced1d54f11a6ab5d7e5b49c7002c3cf76
LEEINWOO <49314154+develinu@users.noreply.github.com>
2024-05-28 14:48:53 +0900

    bugfix: 오탈자 수정

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="datagen_kuma-kuma",
+    name="datagen_kuma",
     version="0.0.1",
     author="devinu",
     author_email="iwlee.dev@gmail.com",
